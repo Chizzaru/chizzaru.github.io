2bb7d145938f0564467ab4776dc3e1792b18030d
Chizzaru <chancernechez261996@gmail.com>
2025-02-06 17:04:28 +0800

    About Me has been added

--- js/appbar.js
+++ js/appbar.js
@@ -1,5 +1,17 @@
 const btn = document.querySelector('.toggle');
 
+const btnAbout = document.getElementById("about");
+const backdropAbout = document.getElementById("backdrop_about");
+const backdropAboutClose = document.getElementById("backdrop_about_close");
+
 btn.addEventListener('click',(e)=>{
     e.currentTarget.classList.toggle('switch');
+})
+
+btnAbout.addEventListener('click',(e)=>{
+    backdropAbout.classList.add("show");
+})
+
+backdropAboutClose.addEventListener('click',(e)=>{
+    backdropAbout.classList.remove("show");
 })
